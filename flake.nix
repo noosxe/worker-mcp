2cc763884c6e84665389aca3b905175139a82399
Levon Kirakosyan <noosx.e@gmail.com>
2026-07-18 17:28:07 +0400

    chore: bump version to 0.1.1 to verify remote flake upgrades

--- flake.nix
+++ flake.nix
@@ -19,14 +19,14 @@
         worker-mcp = let
           pnpmDeps = pkgs.fetchPnpmDeps {
             pname = "worker-mcp-deps";
-            version = "0.1.0";
+            version = "0.1.1";
             src = ./.;
             fetcherVersion = 4;
             hash = "sha256-5t/vaqqwpNisuHvm82a8kGQuUcxbzkJFvPkMZ8GL8Pw=";
           };
         in pkgs.stdenv.mkDerivation {
           pname = "worker-mcp";
-          version = "0.1.0";
+          version = "0.1.1";
           src = ./.;
 
           inherit pnpmDeps;
