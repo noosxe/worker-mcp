dc6af39a42b3e51305a5345aa2decf5c28d9dccc
Levon Kirakosyan <noosx.e@gmail.com>
2026-08-23 08:57:04 +0400

    chore(flake): update pnpmDeps hash for biome 2.5.9

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f system nixpkgs.legacyPackages.${system});
 
       version = "0.4.1";
-      pnpmHash = "sha256-FiD2rDkktqN97fX18y+dUHh1rzs8r9jzTFvTy8sH688=";
+      pnpmHash = "sha256-MpEMJWgPbw9gIHhxowdGH8WFKiWsvFV3fLHOfldTA/M=";
 
       worker-mcp-for =
         pkgs:
